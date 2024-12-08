239e8335335fb83e3309e1bd95f726a9e39fab32
Rick Mukherjee <mukherjeerick347@gmail.com>
2024-12-09 00:27:19 +0530

    Qr genetor

---
+++ main.py
@@ -0,0 +1,15 @@
+import qrcode
+
+print("Enter the website you need to make QR Code")
+s = input()
+
+# QR code box structure
+q = qrcode.QRCode(version=1, box_size=10, border=5)
+
+# adding data and fit it.
+q.add_data(s)
+q.make(fit=True)
+
+# importing as a image and save it in static folder as jpg format
+img = q.make_image(fill="black", back_color="white")
+img.save("qrcode.jpg")
